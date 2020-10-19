c292ee396a7fcfb759bee0904a2f19dc076d9bc0
Adriano <95adriano@gmail.com>
2020-10-19 13:59:17 +0200

    TSP:Updated local comment

--- MOHLevelViewer/TSP.h
+++ MOHLevelViewer/TSP.h
@@ -117,7 +117,7 @@ typedef struct TSPCollisionKDTreeNode_s {
     short Child0; // If Child0 < 0 => Leaf and it represents ~NumFaces, Else Child Plane Index Number
     short Child1; // Child Plane Index Number
     short MaxX; // Axis extent, If Child1 < 0 Z axis else XAxis
-    short MaxZ; // Unknown
+    short MaxZ; // Unknown, this should be a pointer to the Property-Set-File
 } TSPCollisionKDTreeNode_t;
 
 typedef struct TSPCollisionFace_s {
