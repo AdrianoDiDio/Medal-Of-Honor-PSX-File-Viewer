b846386b437aa7bc2577689a322b33cbf452a9bf
AdrianoDiDio <95adriano@gmail.com>
2026-02-23 20:49:40 +0100

    Initial code to parse the script data, added utility functions to handle stack values, added initial code to use simple ops

--- src/Common/BSDScript.c
+++ src/Common/BSDScript.c
@@ -21,19 +21,42 @@
 
 #include "BSDScript.h"
 
-const BSDScriptStackData_t BSDStack[BSD_SCRIPT_STACK_SIZE];
+BSDScriptStackData_t BSDStack[BSD_SCRIPT_STACK_SIZE];
+BSDScriptStackData_t BSDPopStack(int *BSDStackPointer)
+{
+    int CurrentValue;
+    CurrentValue = *BSDStackPointer;
+    (*BSDStackPointer)--;
+    return BSDStack[CurrentValue];
+}
+
+void BSDPushStack(const BSDStackDataType_t Type, void *Data, int *BSDStackPointer)
+{
+    int CurrentValue;
+    CurrentValue = *BSDStackPointer;
+    (*BSDStackPointer)--;
+    BSDStack[CurrentValue].Type = Type;
+    BSDStack[CurrentValue].Data = Data;
+}
+
 
 void BSDScriptDump(FILE *BSDFile, int EntryPointOffset) {
     Byte OpCode;
+    int Pc;
+    int Sp;
+    BSDScriptStackData_t a;
+    BSDScriptStackData_t b;
 
     if (!BSDFile) {
         DPrintf("BSDScriptDump: Invalid file\n");
         return;
     }
-    fseek(BSDFile, EntryPointOffset, SEEK_SET);
-    fread(&OpCode, sizeof(OpCode), 1, BSDFile);
-
+    Pc = EntryPointOffset;
+    Sp = 0;
     while (1) {
+        fseek(BSDFile, Pc, SEEK_SET);
+        fread(&OpCode, sizeof(OpCode), 1, BSDFile);
+        Pc += sizeof(OpCode);
         switch (OpCode) {
             case OP_NOP:
                 break;
@@ -42,12 +65,23 @@ void BSDScriptDump(FILE *BSDFile, int EntryPointOffset) {
             case OP_STORE:
                 break;
             case OP_EQ:
+                b = BSDPopStack(&Sp);
+                a = BSDPopStack(&Sp);
+                BSDPushStack(BSD_STACK_DATA_TYPE_BOOL, (void *)((int )a.Data == (int) b.Data), &Sp);
                 break;
             case OP_NE:
+                b = BSDPopStack(&Sp);
+                a = BSDPopStack(&Sp);
+                BSDPushStack(BSD_STACK_DATA_TYPE_BOOL, (void *)((int )a.Data != (int) b.Data), &Sp);
                 break;
             case OP_OR:
+                b = BSDPopStack(&Sp);
+                a = BSDPopStack(&Sp);
+                BSDPushStack(BSD_STACK_DATA_TYPE_BOOL, (void *)(a.Data != 0 || b.Data != 0), &Sp);
                 break;
             case OP_NOT:
+                a = BSDPopStack(&Sp);
+                BSDPushStack(BSD_STACK_DATA_TYPE_BOOL, (void *)(!a.Data), &Sp);
                 break;
             case OP_LT:
                 break;
@@ -58,12 +92,29 @@ void BSDScriptDump(FILE *BSDFile, int EntryPointOffset) {
             case OP_GE:
                 break;
             case OP_ADD:
+                b = BSDPopStack(&Sp);
+                a = BSDPopStack(&Sp);
+                //TODO(Adriano): If we have floats (and we should not) then this is not true
+                BSDPushStack(BSD_STACK_DATA_TYPE_INT, (void *)((int) a.Data + (int) b.Data != 0), &Sp);
                 break;
             case OP_SUB:
+                b = BSDPopStack(&Sp);
+                a = BSDPopStack(&Sp);
+                //TODO(Adriano): If we have floats (and we should not) then this is not true
+                BSDPushStack(BSD_STACK_DATA_TYPE_INT, (void *)((int) a.Data - (int) b.Data != 0), &Sp);
                 break;
             case OP_MUL:
+                b = BSDPopStack(&Sp);
+                a = BSDPopStack(&Sp);
+                //TODO(Adriano): If we have floats (and we should not) then this is not true
+                BSDPushStack(BSD_STACK_DATA_TYPE_INT, (void *)((int) a.Data * (int) b.Data != 0), &Sp);
                 break;
             case OP_DIV:
+                b = BSDPopStack(&Sp);
+                a = BSDPopStack(&Sp);
+                //TODO(Adriano): If we have floats (and we should not) then this is not true
+                //TODO(Adriano): Check for div by zero too
+                BSDPushStack(BSD_STACK_DATA_TYPE_INT, (void *)((int) a.Data / (int) b.Data != 0), &Sp);
                 break;
             case OP_JZ:
                 break;
